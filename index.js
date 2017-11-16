3144551b07bdb63bd9252749ac80a8932e0dbc43
jamieberrier <berrier.jamie@gmail.com>
2017-11-16 21:55:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ return newDrivers;
 }
 
 function nameToAttributes(drivers){
+  const splitArray = drivers.map(function(element){
+    return element.split(" ");
+   });
 
+   const newArray = splitArray.map(function(element){
+     return Object.assign ({firstName:element[0], lastName:element[1]});
+   });
+return newArray;
 }
 
 function attributesToPhrase(drivers){
