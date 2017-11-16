a120b819382ac9cc025150c64cef539b39a4f803
jamieberrier <berrier.jamie@gmail.com>
2017-11-16 21:53:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 // Code your solution in this file.
 lowerCaseDrivers(){
-  
+  const newDrivers = drivers.map(function(element){
+    return element.toLowerCase();
+  });
+return newDrivers;
 }
 
 nameToAttributes(){
-  
+
 }
 
 attributesToPhrase(){
